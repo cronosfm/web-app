ad5220ea340cae4e4d6875be7aaaa20422b6d8b7
Angel Rodriguez <AngelRodriguez@Roadwego.com>
2021-06-21 20:16:22 -0500

    fix

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -9,19 +9,20 @@ class DeployController extends Controller
 {
     public function deploy(Request $request)
     {
-        $githubPayload = $request->getContent();
-        $githubHash = $request->header('X-Hub-Signature');
+        return 'hola';
+        // $githubPayload = $request->getContent();
+        // $githubHash = $request->header('X-Hub-Signature');
  
-        $localToken = config('app.deploy_secret');
-        $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
-        if (hash_equals($githubHash, $localHash)) {
-            $root_path = base_path();
-            // $process = new Process('cd ' . $root_path . '; ./deploy.sh');
-            // $process = new Process('cd /root/web-app || git pull');
-            $process = new Process('git -c /root/web-app/ pull origin master');
-            $process->run(function ($type, $buffer) {
-                echo $buffer;
-            });
-        }
+        // $localToken = config('app.deploy_secret');
+        // $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
+        // if (hash_equals($githubHash, $localHash)) {
+        //     $root_path = base_path();
+        //     // $process = new Process('cd ' . $root_path . '; ./deploy.sh');
+        //     // $process = new Process('cd /root/web-app || git pull');
+        //     $process = new Process('git -c /root/web-app/ pull origin master');
+        //     $process->run(function ($type, $buffer) {
+        //         echo $buffer;
+        //     });
+        // }
     }
 }
