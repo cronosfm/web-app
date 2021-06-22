2b3dc069dcb1212f2361fbc1bdf719c5afed31a6
Angel Rodriguez <AngelRodriguez@Roadwego.com>
2021-06-21 19:51:12 -0500

    fix

--- app/Http/Controllers/DeployController.php
+++ app/Http/Controllers/DeployController.php
@@ -14,11 +14,10 @@ class DeployController extends Controller
  
         $localToken = config('app.deploy_secret');
         $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
- 
         if (hash_equals($githubHash, $localHash)) {
             $root_path = base_path();
-            // $process = new Process('cd ' . $root_path . '; ./deploy.sh');
-            $process = new Process('cd /root/web-app || git pull');
+            $process = new Process('cd ' . $root_path . '; ./deploy.sh');
+            // $process = new Process('cd /root/web-app || git pull');
             $process->run(function ($type, $buffer) {
                 echo $buffer;
             });
