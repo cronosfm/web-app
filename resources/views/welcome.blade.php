34ae1e8116df7ee6d3ea0a2c87e208273f7906e7
Angel Rodriguez <AngelRodriguez@Roadwego.com>
2021-06-29 11:44:10 -0500

    asdasd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravela
+                    Laravel chido
                 </div>
 
                 <div class="links">
